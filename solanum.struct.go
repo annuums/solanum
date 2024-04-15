13e68f35aa1f319008e6f94e771a7e5de1a48220
Eugene <dev.whoan@gmail.com>
2024-04-15 15:19:03 +0900

    Update Pre Middlewares and Post Middlewares

--- solanum.struct.go
+++ solanum.struct.go
@@ -8,9 +8,10 @@ import (
 
 type (
 	SolaModule struct {
-		uri         string
-		controllers []Controller
-		middlewares []gin.HandlerFunc
+		uri             string
+		controllers     []Controller
+		preMiddlewares  []gin.HandlerFunc
+		postMiddlewares []gin.HandlerFunc
 	}
 
 	SolaController struct {
@@ -18,10 +19,9 @@ type (
 	}
 
 	SolaService struct {
-		Uri        string
-		Method     string
-		Handler    gin.HandlerFunc
-		Middleware gin.HandlerFunc
+		Uri     string
+		Method  string
+		Handler gin.HandlerFunc
 	}
 
 	runner struct {
@@ -34,17 +34,27 @@ type (
 // NewModule 새로운 모듈을 만듭니다. 이 때, 요청받은 router의 uri가 이미 등록되어 있다면 panic
 func NewModule(uri string) *SolaModule {
 	return &SolaModule{
-		uri:         uri,
-		controllers: []Controller{},
-		middlewares: []gin.HandlerFunc{},
+		uri:             uri,
+		controllers:     []Controller{},
+		preMiddlewares:  []gin.HandlerFunc{},
+		postMiddlewares: []gin.HandlerFunc{},
 	}
 }
 
-func (m *SolaModule) GlobalMiddlewares() []gin.HandlerFunc {
-	return m.middlewares
+func (m *SolaModule) PreMiddlewares() []gin.HandlerFunc {
+	return m.preMiddlewares
 }
-func (m *SolaModule) SetGlobalMiddleware(middlewares ...gin.HandlerFunc) {
-	m.middlewares = append(m.middlewares, middlewares...)
+
+func (m *SolaModule) PostMiddlewares() []gin.HandlerFunc {
+	return m.postMiddlewares
+}
+
+func (m *SolaModule) SetPreMiddleware(middlewares ...gin.HandlerFunc) {
+	m.preMiddlewares = append(m.preMiddlewares, middlewares...)
+}
+
+func (m *SolaModule) SetPostMiddleware(middlewares ...gin.HandlerFunc) {
+	m.postMiddlewares = append(m.postMiddlewares, middlewares...)
 }
 
 func (m *SolaModule) Controllers() []Controller {
@@ -59,21 +69,25 @@ func (m *SolaModule) SetRoutes(router *gin.RouterGroup) {
 		services := (c).Handlers()
 
 		for _, svc := range services {
+			handler := m.PreMiddlewares()
+			handler = append(handler, svc.Handler)
+			handler = append(handler, m.PostMiddlewares()...)
+
 			switch svc.Method {
 			case http.MethodGet:
-				router.GET(svc.Uri, svc.Handler)
+				router.GET(svc.Uri, handler...)
 			case http.MethodPost:
-				router.POST(svc.Uri, svc.Handler)
+				router.POST(svc.Uri, handler...)
 			case http.MethodPut:
-				router.PUT(svc.Uri, svc.Handler)
+				router.PUT(svc.Uri, handler...)
 			case http.MethodPatch:
-				router.PATCH(svc.Uri, svc.Handler)
+				router.PATCH(svc.Uri, handler...)
 			case http.MethodDelete:
-				router.DELETE(svc.Uri, svc.Handler)
+				router.DELETE(svc.Uri, handler...)
 			case http.MethodHead:
-				router.HEAD(svc.Uri, svc.Handler)
+				router.HEAD(svc.Uri, handler...)
 			case http.MethodOptions:
-				router.OPTIONS(svc.Uri, svc.Handler)
+				router.OPTIONS(svc.Uri, handler...)
 			default:
 				log.Fatalf("Unknown method registered: %v", svc)
 			}
