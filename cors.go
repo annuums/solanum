c74d4f6e2d846aa53ce7591f3bb2cf09b0c3d123
Eugene <65178775+dev-whoan@users.noreply.github.com>
2024-08-29 15:25:29 +0900

    Use OriginFunc with defined func or given urls (#16)
    * fix cors func; only use origin func based on urls and defined func
    
    * Use log instead of fmt

--- cors.go
+++ cors.go
@@ -1,5 +1,7 @@
 package solanum
 
+import "log"
+
 type CorsOption struct {
 	Urls             []string
 	Headers          []string
@@ -51,10 +53,6 @@ func CorsOptions(opts ...func(*CorsOption)) *CorsOption {
 		opt(&options)
 	}
 
-	if options.Urls == nil || len(options.Urls) == 0 {
-		options.Urls = []string{"*"}
-	}
-
 	if options.Headers == nil || len(options.Headers) == 0 {
 		options.Headers = CorsDefaultHeaders
 	}
@@ -63,18 +61,28 @@ func CorsOptions(opts ...func(*CorsOption)) *CorsOption {
 		options.Methods = CorsDefaultMethods
 	}
 
+	// Define origin func based on urls...
 	if options.OriginFunc == nil {
-		options.OriginFunc = func(origin string) bool {
-			if len(options.Urls) == 1 && options.Urls[0] == "*" {
+		// Allow All
+		if options.Urls == nil && len(options.Urls) == 0 {
+			log.Println("Both urls and originfunc for cors is not defined. allow '*'...")
+			options.OriginFunc = func(origin string) bool {
 				return true
 			}
-
-			for _, allowedOrigin := range options.Urls {
-				if origin == allowedOrigin {
+		} else {
+			// Allow only defined urls
+			options.OriginFunc = func(origin string) bool {
+				if len(options.Urls) == 1 && options.Urls[0] == "*" {
 					return true
 				}
+
+				for _, allowedOrigin := range options.Urls {
+					if origin == allowedOrigin {
+						return true
+					}
+				}
+				return false
 			}
-			return false
 		}
 	}
 
